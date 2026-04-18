3bf203747fa9fa9813a9d6c081d9c4b46630485b
JawadJameel-jj <jawadjameeljj.786@gmail.com>
2026-04-18 18:04:20 +0500

    Add files via upload

---
+++ srape.py
@@ -0,0 +1,49 @@
+from selenium.webdriver import Remote, ChromeOptions as Options
+from selenium.webdriver.chromium.remote_connection import ChromiumRemoteConnection
+from bs4 import BeautifulSoup, soup, soup
+
+SBR_WEBDRIVER = "https://brd-customer-hl_eff51270-zone-is_project:t7rkea9aylu6@brd.superproxy.io:9515"
+
+def scrape_website(website):
+    print("Launching chrome browser ... ")
+    sbr_connection = ChromiumRemoteConnection(SBR_WEBDRIVER, 'goog', 'chrome')
+    
+    with Remote(sbr_connection, options=Options()) as driver:
+        driver.get(website)
+
+        print('Waiting captcha to solve ... ')
+        solve_res = driver.execute('executeCdpCommand', {
+            'cmd': 'Captcha.waitForSolve',
+            'params': {'detectTimeout': 10000},
+        })
+
+        print('Captcha solve status:', solve_res['value']['status'])
+        print('Navigated! Scraping page content ... ')
+        
+        html = driver.page_source   # ✅ NOW it's valid
+        return html                 # ✅ return before session closes
+    
+
+def extract_body_content(html_content):
+    soup = BeautifulSoup(html_content, "html.parser")
+    body_content = soup.body
+    if body_content:
+        return str(body_content)
+    return ""
+
+def clean_body_content(body_content):
+    soup = BeautifulSoup(body_content, "html.parser")
+
+    for script_or_style in soup(["script", "style"]):
+        script_or_style.extract()
+
+    cleaned_content = soup.get_text(separator="\n")
+    cleaned_content = "\n".join(
+    line.strip() for line in cleaned_content.splitlines() if line.strip())
+
+    return cleaned_content
+
+def split_dom_content(dom_content, max_length=6000):
+    return [
+        dom_content[i : i + max_length] for i in range(0, len(dom_content), max_length)
+    ]
